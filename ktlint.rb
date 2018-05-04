49c6e313ec836c312975901b670fd9e76eb117bd
Stanley Shyiko <stanley.shyiko@gmail.com>
2018-05-04 11:43:12 -0700

    0.23.1

--- ktlint.rb
+++ ktlint.rb
@@ -1,8 +1,8 @@
 class Ktlint < Formula
   desc "Kotlin linter"
   homepage "https://github.com/shyiko/ktlint"
-  url "https://github.com/shyiko/ktlint/releases/download/0.23.0/ktlint", :using => :nounzip
-  sha256 "9dbeeda80d7a400a4e698ff09284f1e90319f82c2149ea5ce66f6dee6743dbf5"
+  url "https://github.com/shyiko/ktlint/releases/download/0.23.1/ktlint", :using => :nounzip
+  sha256 "39327a36ee59b28820e067012eb2d0150204656bcdfe40ddd4207c5fab6df258"
   def install
     bin.install "ktlint"
   end
